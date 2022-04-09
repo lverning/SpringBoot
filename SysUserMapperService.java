4fe52f5c912ea293c08dba1fe076a975684b9904
Lverning <74942379+lverning@users.noreply.github.com>
2022-04-10 00:06:50 +0800

    Add files via upload

--- SysUserMapperService.java
+++ SysUserMapperService.java
@@ -6,8 +6,7 @@ import com.springboottest01.mapper.SysUserMapper;
 import com.springboottest01.model.SysUser;
 import com.springboottest01.utlis.JwtUtils;
 import com.springboottest01.vo.Constants;
-import com.springboottest01.vo.ResultReponse;
-import org.springframework.beans.BeanUtils;
+import com.springboottest01.vo.ResultResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,43 +24,87 @@ public class SysUserMapperService {
     @Autowired
     private SysUserMapper sysUserMapper;
 
-    public ResultReponse allSysUser() {
+    /**
+     * 查询所有用户
+     *
+     * @return
+     */
+    public ResultResponse allSysUser() {
         List<SysUser> sysusers = sysUserMapper.selectList(null);
         if (sysusers != null) {
-            return ResultReponse.success(Constants.CODE_200, sysusers);
+            return ResultResponse.success(Constants.CODE_200, sysusers);
         } else {
-            return ResultReponse.error(Constants.CODE_401, "查询出错");
+            return ResultResponse.error(Constants.CODE_401, "查询出错");
         }
     }
 
-    public ResultReponse deleteSysUser(Integer id) {
+    /**
+     * 删除用户
+     *
+     * @param id 用户id
+     * @return
+     */
+    public ResultResponse deleteSysUser(Integer id) {
         if (id == 0) {
-            return ResultReponse.error(Constants.CODE_401, "参数出错");
+            return ResultResponse.error(Constants.CODE_401, "参数出错");
         }
         int deleteById = sysUserMapper.deleteById(id);
         if (deleteById != 0) {
-            return ResultReponse.success(Constants.CODE_200, null);
+            return ResultResponse.success(Constants.CODE_200, null);
         } else {
-            return ResultReponse.error(Constants.CODE_401, "删除失败");
+            return ResultResponse.error(Constants.CODE_401, "删除失败");
         }
     }
 
-    public ResultReponse login(String username, String password) {
+    /**
+     * 用户登录
+     *
+     * @param phone 用户名
+     * @param password 用户密码
+     * @return
+     */
+    public ResultResponse login(String phone, String password) {
         QueryWrapper<SysUser> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("username", username);
+        queryWrapper.eq("phone", phone);
         queryWrapper.eq("password", password);
         try {
             SysUser selectOne = sysUserMapper.selectOne(queryWrapper);
             if (selectOne != null) {
-                String token = JwtUtils.createToken(String.valueOf(selectOne.getId()), selectOne.getUsername());
-                return ResultReponse.success(Constants.CODE_200,token);
-            }else {
+                String token = JwtUtils.createToken(String.valueOf(selectOne.getId()), selectOne.getPhone());
+                return ResultResponse.success(Constants.CODE_200, token);
+            } else {
                 throw new ServiceException(Constants.CODE_401, "用户名或密码错误");
             }
         } catch (Exception e) {
             throw new ServiceException(Constants.CODE_401, "用户名或密码错误");
         }
+    }
 
-
+    /**
+     * 用户注册
+     *
+     * @param user
+     * @return
+     */
+    public ResultResponse insertUser(SysUser user) {
+        if (user.getUsername() == null || "".equals(user.getUsername())
+                || user.getPassword() == null || "".equals(user.getPassword())
+                || user.getPhone() == null || "".equals(user.getPhone())) {
+            throw new ServiceException(Constants.CODE_401, "信息不能为空");
+        }
+        QueryWrapper<SysUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("phone", user.getPhone());
+//        SysUser selectOne = sysUserMapper.selectOne(queryWrapper);
+        if (sysUserMapper.selectOne(queryWrapper) == null) {
+            int insert = sysUserMapper.insert(user);
+            if (insert != 0) {
+                String token = JwtUtils.createToken(String.valueOf(user.getId()), user.getUsername());
+                return ResultResponse.success(Constants.CODE_200, token);
+            } else {
+                throw new ServiceException(Constants.CODE_401, "注册失败");
+            }
+        } else {
+            throw new ServiceException(Constants.CODE_401, "手机号重复");
+        }
     }
 }
