1e8b99490355daf934ec5066ebb8e63c49655056
Lverning <74942379+lverning@users.noreply.github.com>
2022-04-16 18:19:10 +0800

    Add files via upload

---
+++ CommodityService.java
@@ -0,0 +1,13 @@
+package com.springboottest01.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.springboottest01.model.Commodity;
+
+/**
+ * @author 吕二宁
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/4/11 23:55
+ */
+public interface CommodityService extends IService<Commodity> {
+}
