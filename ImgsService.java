05c442223be1fa4ced71ebca39925a52848cf16c
Lverning <74942379+lverning@users.noreply.github.com>
2022-04-17 00:24:28 +0800

    Add files via upload

---
+++ ImgsService.java
@@ -0,0 +1,14 @@
+package com.springboottest01.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.springboottest01.model.Imgs;
+
+/**
+ * @author 吕二宁
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/4/10 20:55
+ */
+public interface ImgsService extends IService<Imgs> {
+
+}
